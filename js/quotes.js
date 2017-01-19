04611d008bf790b04383f89cc790d0387fe3f49f
PirieD704 <dp75975@gmail.com>
2017-01-19 14:26:31 -0500

    removed circle nav and about to start side nav, added quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,1 +1,96 @@
-const 
+const quoteArray = [
+	{
+		quote: "Heart and head are the constituent parts of character; temperament has almost nothing to do with it, and, therefore, character is dependent upon education, and is susceptible of being corrected and improved.",
+		author: "Giacomo Casanova"
+	},{
+		quote: "How lucky I am to have something that makes saying goodbye so hard.",
+		author: "A. A. Milne"
+	},{
+		quote: "Man cannot remake himself without suffering for he is both the marble and the sculptor.",
+		author: "Alexis Carrel"
+	},{
+		quote: "The past is a textbook of tyrants, while the future is the bible of the free.",
+		author: "Herman Melville"
+	},{
+		quote: "In command and out of control.",
+		author: "Kevin Kelly"
+	},{
+		quote: "Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.",
+		author: "Dr. Seuss"
+	},{
+		quote: 'A conversation between Hermann Goering and Gustave Gilbert published in Gilbert\'s Nuremberg Diary: We got around to the subject of war again and I said that, contrary to his attitude, I did not think that the common people are very thankful for leaders who bring them war and destruction. "Why, of course, the people don\'t want war," Goering shrugged. "Why would some poor slob on a farm want to risk his life in a war when the best that he can get out of it is to come back to his farm in one piece. Naturally, the common people don\'t want war; neither in Russia nor in England nor in America, nor for that matter in Germany. That is understood. But, after all, it is the leaders of the country who determine the policy and it is always a simple matter to drag the people along, whether it is a democracy or a fascist dictatorship or a Parliament or a Communist dictatorship." "There is one difference," I pointed out. "In a democracy the people have some say in the matter through their elected representatives, and in the United States only Congress can declare wars." "Oh, that is all well and good, but, voice or no voice, the people can always be brought to the bidding of the leaders. That is easy. All you have to do is tell them they are being attacked and denounce the pacifists for lack of patriotism and exposing the country to danger. It works the same way in any country."',
+		author: "Hermann Goering"
+	},{
+		quote: "Stand in the truth",
+		author: "Suze Orman"
+	},{
+		quote: "Men fear thought as they fear nothing else on earth---more than ruin---more even than death... Thought is subversive and revolutionary, destructive and terrible, thought is merciless to privilege, established institutions, and comfortable habit.  Thought looks into the pit of hell and is not afraid. Thought is great and swift and free, the light of the world, and the chief glory of man.",
+		author: "Bertrand Russell"
+	},{
+		quote: "Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway.",
+		author: "Earl Nightingale"
+	},{
+		quote: "People seldom improve when they have no other model but themselves to copy.",
+		author: "Oliver Goldsmith"
+	},{
+		quote: "Good judgment comes from experience, and experience comes from bad judgment.",
+		author: "Barry LePatner"
+	},{
+		quote: "Success is the sum of small efforts, repeated day in and day out.",
+		author: "Robert Collier"
+	},{
+		quote: "I will tell you how to become rich. Close the doors. Be fearful when others are greedy. Be greedy when others are fearful.",
+		author: "Warren Buffet"
+	},{
+		quote: "Confidence is quiet, insecurities are loud.",
+		author: "Unknown"
+	},{
+		quote: "The only person who cannot be helped is that person who blames others.",
+		author: "Carl Rogers"
+	},{
+		quote: "Nothing is certain but death and taxes.",
+		author: "Benjamin Franklin"
+	},{
+		quote: "Nothing is infinite but the universe and human stupidity, and I'm not too sure about the universe...",
+		author: "Albert Einstein"
+	},{
+		quote: "Fear is your best friend or your worst enemy. It's like fire. If you can control it, it can cook for you; it can hear your house. If you can't control it, it will burn everything around you and destroy you.",
+		author: "Mike Tyson"
+	},{
+		quote: "Everything you've ever wanted is on the other side of fear.",
+		author: "George Adair"
+	},{
+		quote: "It is good to have an end to journey towards, but it is the journey that matters in the end.",
+		author: "Ernest Hemingway"
+	},{
+		quote: "A formal education will get you a job, but a self-education will earn you a fortune.",
+		author: "Unknown"
+	},{
+		quote: "A society grows great when an old man plants trees whose shade he knows he will never sit in.",
+		author: "Greek Proverb"
+	},{
+		quote: "Any man is liable to make mistakes, but only a fool perseveres in error",
+		author: "Cicero"
+	},{
+		quote: "I am, somehow, less interested in the weight and convolutions of Einstein’s brain than in the near certainty that people of equal talent have lived and died in cotton fields and sweatshops.",
+		author: "Stephen Jay Gould"
+	},{
+		quote: "You only live once – but if you work it right, once is enough.",
+		author: "Joe E. Lewis"
+	},{
+		quote: "Anyone who stops learning is old, whether at twenty or eighty. Anyone who keeps learning stays young. The greatest thing in life is to keep your mind young.",
+		author: "Henry Ford"
+	},{
+		quote: "The man who removes a mountain begins by carrying away small stones.",
+		author: "Chinese Proverb"
+	},{
+		quote: "Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius – and a lot of courage – to move in the opposite direction.",
+		author: "E.F. Schumacker"
+	},{
+		quote: "Putting off an easy thing makes it hard. Putting off a hard thing makes it impossible.",
+		author: "George Claude Lorimer"
+	},{
+		quote: "The person interested in success has to learn to view failure as a healthy, inevitable part of the process of getting to the top.",
+		author: "Dr. Joyce Brothers"
+	}
+]

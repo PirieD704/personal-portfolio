07a2646ba733595858122fb039abc091f184bf31
PirieD704 <dp75975@gmail.com>
2017-02-20 15:34:04 -0500

    added features on contact page and deleted unessasary files

--- js/controller.js
+++
@@ -1,4 +0,0 @@
-var portfolio = angular.module('portfolio', []);
-portfolio.controller('portfolioController', ['$scope', '$timeout', function($scope, $timeout){
-	$scope.test = "it's still working"
-}
